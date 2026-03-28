c63cd4463cfbbdd16e440c057a708674eee6143f
JIMXPS13\jimwo <jimwooley@hotmail.com>
2026-03-27 22:00:58 -0400

    Adding fakes, DI and updating create to be testable

---
+++ TestProject1/Fakes/FakeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Data;
+
+namespace TestProject1.Fakes;
+
+internal class FakeDbContext : ApplicationDbContext
+{
+    public static FakeDbContext ContextFactory()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+            .Options;
+        return new FakeDbContext(options);
+
+    }
+    public FakeDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public int SaveCount { get; set; }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SaveCount += 1;
+        return Task.FromResult(SaveCount);
+    }
+}
